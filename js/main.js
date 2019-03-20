5637ec5310fe8a8e5e5b6a98145d7959d5023b84
chris <cleon5886@gmail.com>
2019-03-20 19:17:18 +0100

    homework

---
+++ js/main.js
@@ -0,0 +1,40 @@
+class Main {
+	execute(){
+		//this is one comment
+		//your code 
+
+		let cities = [ "Paris" , "Madrid" , "Barcelona", "London" , "Berlin"];
+
+		var Madrid = "Madrid"
+		var Paris = "Paris"
+		var Barcelona = "Barcelona"
+		var London = "London"
+		var Berlin = "Berlin"
+
+		function getcity(index) {
+		    if (cities === 0){
+		        return paris;
+		        }
+
+		    if (cities === 1){
+		        return Madrid;
+		        }
+
+		    if (cities === 2){
+		        return Barcelona;
+		        }
+
+		    if (cities === 3){
+		       return London;
+		       }
+
+		    if (cities === 4){
+		            return Berlin;
+		        }
+		}        
+		getcities(1);
+	}
+
+}
+
+new Main().execute();
